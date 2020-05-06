650c14ed8c4d0e2530e6f0d16a324a014ddca770
donchandu123 <shubhamramtekecjc@gmail.com>
2020-05-06 13:09:18 +0530

    Shubham commiting

--- src/main/java/com/example/demo/conttroller/AddController.java
+++ src/main/java/com/example/demo/conttroller/AddController.java
@@ -4,6 +4,14 @@ public class AddController {
 
 	public String HelloAdmin()
 	{
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+
+		AddController add=new AddController();
+=======
+>>>>>>> 6e145f0... 1 st Commit of the Day]
+>>>>>>> be67e56... Commiting to master branch
 		return "Hello Shubham Your Code ius ready for commit";
 		
 	}
