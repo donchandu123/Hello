b983bba41d56bce5469e0914e69fdfd21a8ac670
donchandu123 <shubhamramtekecjc@gmail.com>
2020-05-06 13:29:11 +0530

    Shubham have Commited on the branch

--- src/main/java/com/example/demo/conttroller/AddController.java
+++ src/main/java/com/example/demo/conttroller/AddController.java
@@ -4,11 +4,10 @@ public class AddController {
 
 	public String HelloAdmin()
 	{
-<<<<<<< HEAD
 
 		AddController add=new AddController();
-=======
->>>>>>> 6e145f0... 1 st Commit of the Day]
+
+
 		return "Hello Shubham Your Code ius ready for commit";
 		
 	}
